26898771f3517f633a1ebc187136fe45bc3f972a
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-01 19:53:03 +0530

    opensearch client with config

---
+++ internal/opensearch/client.go
@@ -0,0 +1,30 @@
+package opensearch
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/Thothica/thothica/cmd/profile"
+	"github.com/opensearch-project/opensearch-go"
+	"github.com/spf13/cobra"
+)
+
+func NewClient() *opensearch.Client {
+	currentProfile := profile.GetCurrentProfile()
+
+	opensearchConfig := &opensearch.Config{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		Addresses: []string{currentProfile.Endpoint},
+		Username:  currentProfile.User,
+		Password:  currentProfile.Password,
+	}
+
+	client, err := opensearch.NewClient(*opensearchConfig)
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+
+	return client
+}
