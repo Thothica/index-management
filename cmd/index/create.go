1c48532f4612a8d8dc24e4cc095ad04ecc55926f
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-15 03:25:16 +0530

    removed bubble Idea changed, now will use reflect on json types to first make a index json from it and give it to user so they can change and then send request with it.

--- cmd/index/create.go
+++ cmd/index/create.go
@@ -1,14 +1,13 @@
 package index
 
 import (
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
 var (
-	Pipeline  string
-	createCmd = &cobra.Command{
+	Pipeline    string
+	VectorIndex string
+	createCmd   = &cobra.Command{
 		Use:   "create",
 		Short: "Creates a index for semantic search.",
 		Long:  `create (thothica index create) is a tool which helps you to define mappings and create index in opensearch to perform semantic search.`,
@@ -20,6 +19,8 @@ var (
 func init() {
 	createCmd.Flags().StringVarP(&Pipeline, "pipeline", "p", "", "Name of embedding pipeline for this index.")
 	createCmd.MarkFlagRequired("pipeline")
+	createCmd.Flags().StringVarP(&VectorIndex, "vector-index", "v", "", "Name of mapping/column which will hold embeddings as defined in the pipeline ")
+	createCmd.MarkFlagRequired("vector-index")
 }
 
 type index struct {
@@ -30,43 +31,4 @@ type index struct {
 	Mappings struct {
 		Properties []map[string]interface{} `json:"properties"`
 	} `json:"mappings"`
-}
-
-type model struct {
-	mappingFieldTypes []string
-	neuralSearchIndex index
-	cursor            int
-	selectionComplete bool
-	fieldInput        textinput.Model
-}
-
-func initialModel() model {
-	input := textinput.New()
-	input.Placeholder = "Input your field name."
-	input.Focus()
-
-	return model{
-		mappingFieldTypes: []string{"binary", "boolean", "ip", "object", "nested", "keyword", "text", "date"},
-		cursor:            0,
-		neuralSearchIndex: index{
-			Settings: struct {
-				IndexKNN        bool   "json:\"index.knn\""
-				DefaultPipeline string "json:\"default_pipeline\""
-			}{IndexKNN: true, DefaultPipeline: Pipeline},
-		},
-		selectionComplete: false,
-		fieldInput:        input,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return nil, nil
-}
-
-func (m model) View() string {
-	return ""
 }
