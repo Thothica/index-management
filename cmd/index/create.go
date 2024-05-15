e6a8284c5dda218448534c4d30b206d6f5a8df67
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-16 01:45:32 +0530

    index create complete. works beautifully

--- cmd/index/create.go
+++ cmd/index/create.go
@@ -7,6 +7,11 @@ import (
 	"os"
 	"strings"
 
+	"github.com/Thothica/thothica/internal/opensearch"
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 )
 
@@ -21,13 +26,17 @@ type index struct {
 }
 
 var (
-	Pipeline    string
-	VectorIndex string
-	Filename    string
-	data        []map[string]interface{}
-    BadFormatError = errors.New("Wrong file format, only json file of format - \n \t[{\"data_key\":\"data_value\", ..}..{}] are accepted")
-	i           index
-	createCmd   = &cobra.Command{
+	Pipeline       string
+	VectorIndex    string
+	Filename       string
+	Index          string
+	IndexBody      string
+	data           []map[string]interface{}
+	i              index
+	magentaStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	c              = opensearch.NewClient()
+	BadFormatError = errors.New("Wrong file format, only json file of format - \n \t[{\"data_key\":\"data_value\", ..}..{}] are accepted")
+	createCmd      = &cobra.Command{
 		Use:   "create",
 		Short: "Creates a index for semantic search.",
 		Long:  `create (thothica index create) is a tool which helps you to define mappings and create index in opensearch to perform semantic search.`,
@@ -47,6 +56,11 @@ var (
 			err = json.NewDecoder(file).Decode(&data)
 			if err != nil {
 				cobra.CheckErr(err)
+			}
+
+			i.Mappings.Properties[VectorIndex] = map[string]interface{}{
+				"type":      "knn_vector",
+				"dimension": 768,
 			}
 
 			for k, v := range data[0] {
@@ -71,7 +85,11 @@ var (
 				cobra.CheckErr(err)
 			}
 
-			fmt.Println(string(IndexJson))
+			IndexBody = string(IndexJson)
+
+			if _, err := tea.NewProgram(initialModel()).Run(); err != nil {
+				cobra.CheckErr(err)
+			}
 		},
 	}
 )
@@ -83,10 +101,102 @@ func init() {
 	createCmd.MarkFlagRequired("vector-index")
 	createCmd.Flags().StringVarP(&Filename, "file", "f", "", "Path of file containing data for semantic search.\n Json file should be of format - \n \t[{\"data_key\":\"data_value\", ..}..{}]")
 	createCmd.MarkFlagRequired("vector-index")
+	createCmd.Flags().StringVarP(&Index, "index", "i", "", "Name of index you are creating.")
+	createCmd.MarkFlagRequired("index")
 }
 
 func InitialseIndex(i *index) {
 	i.Settings.IndexKNN = true
 	i.Settings.DefaultPipeline = Pipeline
 	i.Mappings.Properties = make(map[string]interface{})
+}
+
+type apiResponse string
+type apiError error
+
+type model struct {
+	textarea    textarea.Model
+	spinner     spinner.Model
+	loading     bool
+	apiResponse string
+}
+
+func CreateIndex(Body, Index string) tea.Cmd {
+	return func() tea.Msg {
+		res, err := c.CreateIndex(Body, Index)
+		if err != nil {
+			return apiError(err)
+		}
+		return apiResponse(res)
+	}
+}
+
+func initialModel() model {
+	t := textarea.New()
+	t.Focus()
+	t.SetHeight(25)
+	t.CharLimit = 0
+	t.SetValue(IndexBody)
+	t.FocusedStyle.CursorLine = magentaStyle
+
+	return model{
+		textarea:    t,
+		apiResponse: "",
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "ctrl+s":
+			m.loading = true
+			return m, tea.Batch(m.spinner.Tick, CreateIndex(m.textarea.Value(), Index))
+		}
+	default:
+		if !m.textarea.Focused() {
+			cmd = m.textarea.Focus()
+			cmds = append(cmds, cmd)
+		}
+
+	case apiError:
+		m.loading = false
+		m.apiResponse = msg.Error()
+		return m, tea.Quit
+
+	case apiResponse:
+		m.loading = false
+		m.apiResponse = string(msg)
+		return m, tea.Quit
+	}
+
+	m.textarea, cmd = m.textarea.Update(msg)
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	var b strings.Builder
+
+	if m.loading {
+		fmt.Fprintf(&b, "\n %s Creating Index...\n\n", m.spinner.View())
+		return b.String()
+	}
+
+	if m.apiResponse != "" {
+		b.WriteString(m.apiResponse)
+		return b.String()
+	}
+
+	fmt.Fprintf(&b, "\n%s\n\n%s\n\n(press ctrl+c to abort or %s to create Index)", magentaStyle.Copy().Render("Edit Index as you like!"), m.textarea.View(), magentaStyle.Copy().Render("ctrl+s"))
+	return b.String()
 }
