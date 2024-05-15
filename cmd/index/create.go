bfed6294cb9a7b0f1ba3d7b503aa6331538e9956
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-15 16:26:54 +0530

    Initialised Index

--- cmd/index/create.go
+++ cmd/index/create.go
@@ -17,11 +17,15 @@ type index struct {
 var (
 	Pipeline    string
 	VectorIndex string
+	Filename    string
+	data        []map[string]interface{}
+	i           index
 	createCmd   = &cobra.Command{
 		Use:   "create",
 		Short: "Creates a index for semantic search.",
 		Long:  `create (thothica index create) is a tool which helps you to define mappings and create index in opensearch to perform semantic search.`,
 		Run: func(cmd *cobra.Command, args []string) {
+			InitialseIndex(&i)
 		},
 	}
 )
@@ -29,16 +33,14 @@ var (
 func init() {
 	createCmd.Flags().StringVarP(&Pipeline, "pipeline", "p", "", "Name of embedding pipeline for this index.")
 	createCmd.MarkFlagRequired("pipeline")
-	createCmd.Flags().StringVarP(&VectorIndex, "vector-index", "v", "", "Name of mapping/column which will hold embeddings as defined in the pipeline ")
+	createCmd.Flags().StringVarP(&VectorIndex, "vector-index", "v", "", "Name of mapping/column which will hold embeddings as defined in the pipeline.")
+	createCmd.MarkFlagRequired("vector-index")
+	createCmd.Flags().StringVarP(&Filename, "file", "f", "", "Path of file containing data for semantic search.\n Json file should be of format - \n \t[{\"data_key\":\"data_value\", ..}..{}]")
 	createCmd.MarkFlagRequired("vector-index")
 }
 
-type index struct {
-	Settings struct {
-		IndexKNN        bool   `json:"index.knn"`
-		DefaultPipeline string `json:"default_pipeline"`
-	} `json:"settings"`
-	Mappings struct {
-		Properties []map[string]interface{} `json:"properties"`
-	} `json:"mappings"`
+func InitialseIndex(i *index) {
+	i.Settings.IndexKNN = true
+	i.Settings.DefaultPipeline = Pipeline
+	i.Mappings.Properties = make(map[string]interface{})
 }
