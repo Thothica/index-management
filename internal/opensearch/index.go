0166cf8deff95b8a9f8fbf6d9e095e24e3314c5c
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-17 02:09:46 +0530

    Added method for Index deletion

--- internal/opensearch/index.go
+++ internal/opensearch/index.go
@@ -22,14 +22,37 @@ func (c Client) CreateIndex(Body, Index string) (string, error) {
 
 	var resp map[string]interface{}
 	err = json.NewDecoder(res.Body).Decode(&resp)
-	resopnse, err := json.MarshalIndent(resp, "", " ")
+	response, err := json.MarshalIndent(resp, "", " ")
 	if err != nil {
 		return "", err
 	}
 
 	if res.Status() != "200 OK" {
-		return "", fmt.Errorf("opensearch returned error\n %s", string(resopnse))
+		return "", fmt.Errorf("opensearch returned error\n %s", string(response))
 	}
 
-	return string(resopnse), nil
+	return string(response), nil
+}
+
+func (c Client) DeleteIndex(Index string) (string, error) {
+	req := opensearchapi.IndicesDeleteRequest{
+		Index: []string{Index},
+	}
+	res, err := req.Do(context.Background(), c.Client)
+	if err != nil {
+		return "", err
+	}
+
+	var resp map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&resp)
+	response, err := json.MarshalIndent(resp, "", " ")
+	if err != nil {
+		return "", err
+	}
+
+	if res.Status() != "200 OK" {
+		return "", fmt.Errorf("opensearch returned error\n %s", string(response))
+	}
+
+	return string(response), nil
 }
